b84ccb77d285144a496eda5c06ff9b65de75ab67
Brandon McConathy <120681452+brandonmcconathy@users.noreply.github.com>
2025-07-17 22:31:09 -0700

    Add files via upload

---
+++ DDL.sql
@@ -0,0 +1,10 @@
+-- Group 12, Car City Sales Management System
+-- Lindsey Clement & Brandon Mcconathy
+
+SET FOREIGN_KEY_CHECKS = 0;
+SET AUTOCOMMIT = 0;
+
+-- SQL Code goes here
+
+SET FOREIGN_KEY_CHECKS = 1;
+COMMIT;
